7c277effe37a00470fa2fba977d9517e86f6feee
megha angadi <meghamangadi@gmail.com>
2021-09-12 21:06:00 +0530

    Added: Welcome Message to the User

---
+++ src/com/bridgelabz/hashtable/HashTableMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.hashtable;
+
+public class HashTableMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Data Structure Hash Tables and Binary search Trees");
+	}
+
+}
